0250ec29724917dea3aeb650e5e3ef536d27fe19
CuongNV2.EDN <vietcuong175@gmail.com>
2022-06-06 14:18:15 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Home = () => (
+  <div className="flexCenter w-full h-screen bg-nft-red-violet">
+    <h1 className="text-3xl font-bold underline text-white">
+      Hello
+    </h1>
+  </div>
+);
+
+export default Home;
